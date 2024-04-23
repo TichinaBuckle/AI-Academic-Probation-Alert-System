a08bbefb71d317948f487327d171f57d81d09c9a
ramouneor <147168223+ramouneor@users.noreply.github.com>
2024-04-23 15:11:21 -0500

    Add files via upload

--- EmailAlert.py
+++ EmailAlert.py
@@ -1,16 +1,16 @@
 import smtplib
 
-sender = "Private Person <mailtrap@demomailtrap.com>"
-receiver = "A Test User <linoone104@gmail.com>"
+sender = "Private Person <from@example.com>"
+receiver = "A Test User <to@example.com>"
 
 message = f"""\
-Subject: Hi Mailtrap
+Subject: Academic Probation Notification
 To: {receiver}
 From: {sender}
 
-This is a test e-mail message."""
+Academic Probation Notification-Do not reply to this email, I hope this message finds you well. I wanted to inform you that, following a review of your academic performance you are currently on academic probation."""
 
-with smtplib.SMTP("live.smtp.mailtrap.io", 587) as server:
+with smtplib.SMTP("sandbox.smtp.mailtrap.io", 2525) as server:
     server.starttls()
-    server.login("api", "c718b4483cd28992ae4089da8297df64")
+    server.login("7bc90479fda654", "288824f564c517")
     server.sendmail(sender, receiver, message)
