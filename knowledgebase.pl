6841c2fcbfb261d723e8a26b914703eb517547b8
WinstonMcleod3580 <45207073+WinstonMcleod3580@users.noreply.github.com>
2024-04-23 10:28:05 -0500

    Update knowledgebase.pl
    update

--- knowledgebase.pl
+++ knowledgebase.pl
@@ -1,8 +1,9 @@
+% Group Members: - Tichina Buckle, Micah Brown, Ramoune Roberts, Winston
+% Date Last Updated: 04-22-2024
+% File Name: KnowledgeBase.pl
+% Description: Contains Knowledge base for gpa
+
 % Dynamic predicates
-:- dynamic(module_details/5).
-:- dynamic(student_master/5).
-:- dynamic(module_master/2).
-:- dynamic(default_gpa/1).
 :- dynamic(total_credits/1).
 :- dynamic(total_gpe/1).
 
@@ -17,73 +18,69 @@
 % module_details(Module, Year, Semester, StudentID, GradePoints)
 % module_details('Artificial Intelligence', 2023, 2000345, 4, 3.67).
 
-% Assert a default GPA value
-:- assert(default_gpa(2.2)).
 
 % Initialize total credits and total grade points
-totalCredits(0).
-totalGpe(0).
+sem1TotalCredits(0).
+sem1TotalGPE(0).
 
-% Main func to get the credits and gp
+% Main function to get the credits and grade points
 calculate :- write('Enter the credits of the module: '), read(Credits),
-            nl, write('Enter the grade point earned: '), read(Grade_Point),
-            calculate_gpe(Credits, Grade_Point, GPE),
-            addCredits(Credits, Total_Credits),
-            addGradePoints(GPE, Total_GPE),
+            nl, write('Enter the grade point earned: '), read(GradePoint),
+            calculateGPE(Credits, GradePoint, GPE),
+            addCredits(Credits, TotalCredits),
+            addGradePoints(GPE, TotalGPE),
             nl, write('Do you want to enter another module? (y/n)'), read(Choice),
-            (Choice = 'y' -> calculate; calculate_gpa(Total_Credits, Total_GPE, GPA),
+            (Choice = 'y' -> calculate; calculateGPA(TotalCredits, TotalGPE, GPA),
             nl, write('Your GPA is: '), write(GPA)).
-
 
 % Calculations
 
 % Total Credits for the modules taken by the student taken in each semester
-totalCredits(StudentId, Year, Sem1_Cred, Sem2_Cred):-
-    findall(Credit, moddet(StudentId, Year, 1, Credit), CreditList1),
-    sum_list(CreditList1, Sem1_Cred),   %Sem1
-    findall(Credit, moddet(StudentId, Year, 2, Credit), CreditList2),
-    sum_list(CreditList2, Sem2_Cred).   %Sem2
+sem1TotalCredits(StudentId, Year, Sem1Cred, Sem2Cred):-
+    findall(Credit, moduleDetails(StudentId, Year, 1, Credit), CreditList1),
+    sumList(CreditList1, Sem1Cred),   %Sem1
+    findall(Credit, moduleDetails(StudentId, Year, 2, Credit), CreditList2),
+    sumList(CreditList2, Sem2Cred).   %Sem2
 
-sum_list([], 0).
-sum_list([H|T], N):- sum_list(T, N2), N is H + N2.
+sumList([], 0).
+sumList([H|T], N):- sumList(T, N2), N is H + N2.
 
-moddet(StudentId, Year, Sem, Credit):-
-    module_details(StudentId, Module, Year, Sem, _),
-    module_master(Module, Credit).
+moduleDetails(StudentId, Year, Sem, Credit):-
+    moduleDetails(StudentId, Module, Year, Sem, _),
+    moduleMaster(Module, Credit).
 
 % Multiply the GP for each module by the corresponding credits to get the [GP earned] for each module
-gp_earned(StudentId, Year, Sem, GPE):-
-    module_details(StudentId, X, Year, Sem, GP),
-    module_master(X, Credit),
+gpEarned(StudentId, Year, Sem, GPE):-
+    moduleDetails(StudentId, X, Year, Sem, GP),
+    moduleMaster(X, Credit),
     GPE is GP * Credit.
 
 % Calculate Total [GP earned] for each semester
-total_gpe_per_sem(StudentId, Year, Sem1Gp, Sem2Gp):-
-    findall(GPE1, gp_earned(StudentId, Year, 1, GPE1), GPE1_List),
-    sum_list(GPE1_List, Sem1Gp),       %Sem1
-    findall(GPE2, gp_earned(StudentId, Year, 2, GPE2), GPE2_List),
-    sum_list(GPE2_List, Sem2Gp).       %Sem2
+totalGPEPerSem(StudentId, Year, Sem1Gp, Sem2Gp):-
+    findall(GPE1, gpEarned(StudentId, Year, 1, GPE1), GPE1List),
+    sumList(GPE1List, Sem1Gp),       %Sem1
+    findall(GPE2, gpEarned(StudentId, Year, 2, GPE2), GPE2List),
+    sumList(GPE2List, Sem2Gp).       %Sem2
 
 % Divide Total [GP earned] by the Total Credits for the module taken in each semester
-total_gpe(Year, Total_gpe_sem1, Total_gpe_sem2, StudentId):-
-    total_gpe_per_sem(StudentId, Year, Sem1Gp, Sem2Gp),
-    totalCredits(StudentId, Year, Sem1_Cred, Sem2_Cred),
-    Total_gpe_sem1 is Sem1Gp / Sem1_Cred,
-    (Sem2_Cred =:= 0 -> Total_gpe_sem2 is 0; Total_gpe_sem2 is Sem2Gp / Sem2_Cred).
+totalGPE(Year, TotalGpeSem1, TotalGpeSem2, StudentId):-
+    totalGPEPerSem(StudentId, Year, Sem1Gp, Sem2Gp),
+    sem1TotalCredits(StudentId, Year, Sem1Cred, Sem2Cred),
+    TotalGpeSem1 is Sem1Gp / Sem1Cred,
+    (Sem2Cred =:= 0 -> TotalGpeSem2 is 0; TotalGpeSem2 is Sem2Gp / Sem2Cred).
 
 % If there is a semester 2, calculate the [cumulative gpa] by
 % dividing the sum of Total [GP earned] for both semesters by the sum of [Total Credits] for both semesters
 % If only semester one data is available then the [cumulative gpa] is the GPA for semester one
-cumulative_gpa(Year, Cum_GPA, StudentId):-
-    total_gpe_per_sem(StudentId, Year, Sem1Gp, Sem2Gp),
-    totalCredits(StudentId, Year, Sem1_Cred, Sem2_Cred),
-    Total_gpe_sem is Sem1Gp + Sem2Gp,
-    Total_credit is Sem1_Cred + Sem2_Cred,
-    Cum_GPA is Total_gpe_sem / Total_credit.
+cumulativeGPA(Year, CumGPA, StudentId):-
+    totalGPEPerSem(StudentId, Year, Sem1Gp, Sem2Gp),
+    sem1TotalCredits(StudentId, Year, Sem1Cred, Sem2Cred),
+    TotalGpeSem is Sem1Gp + Sem2Gp,
+    TotalCredit is Sem1Cred + Sem2Cred,
+    CumGPA is TotalGpeSem / TotalCredit.
 
 % Get students' information including GPA
-get_students(Year, Id, Name, Total_gpe_sem1, Total_gpe_sem2, CGPA):-
-    student_master(Id, Name, _, _, _),
-    total_gpe(Year, Total_gpe_sem1, Total_gpe_sem2, Id),
-    cumulative_gpa(Year, CGPA, Id),!.
-    % nl, write(Id), write(Name), tab(2), write(Total_gpe_sem1), tab(2), write(Total_gpe_sem2), tab(2), write(CGPA),!.
+getStudents(Year, Id, Name, TotalGpeSem1, TotalGpeSem2, CGPA):-
+    studentMaster(Id, Name, _, _, _),
+    totalGPE(Year, TotalGpeSem1, TotalGpeSem2, Id),
+    cumulativeGPA(Year, CGPA, Id),!.
